85a326e2d41c8ae9d4b50c6b5680befabebf3ed7
DasithJ <dasith.jayanandana@ebuilder.com>
2024-01-10 08:38:23 +0530

    Updated details

--- src/components/achive/Menu.jsx
+++ src/components/achive/Menu.jsx
@@ -1,45 +1,59 @@
-import Work1 from "../../assets/postman.PNG";
-import Work2 from "../../assets/firebase.PNG";
-import Work3 from "../../assets/introMobile.PNG";
-import Work4 from "../../assets/versionControl.PNG";
-import Work5 from "../../assets/net.PNG";
-import Work6 from "../../assets/kotlin.PNG";
+import Work1 from "../../assets/cloud_quest.png";
+import Work2 from "../../assets/aws.png";
+import Work3 from "../../assets/postman.PNG";
+import Work4 from "../../assets/firebase.PNG";
+import Work5 from "../../assets/introMobile.PNG";
+import Work6 from "../../assets/versionControl.PNG";
+import Work7 from "../../assets/net.PNG";
+import Work8 from "../../assets/kotlin.PNG";
 
 const Menu = [
   {
     id: 1,
     image: Work1,
-    title: "Postman API Fundamentals Student Expert",
-    link: "https://badgr.com/public/assertions/eITuYwlOR12xKbFdUPC6jg?identity__email=dasithjaya99@gmail.com",
+    title: "AWS Cloud Quest: Cloud Practitioner",
+    link: "https://www.credly.com/badges/6cf35b74-e5b5-4ea9-9f12-8b8e87a02c30/linked_in?t=s5agq8",
   },
   {
     id: 2,
     image: Work2,
-    title: "Serverless Firebase Development",
-    link: "https://www.cloudskillsboost.google/public_profiles/586bee6d-8079-42e3-9949-b1adff81b189/badges/4778184?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
+    title: "Integrating AWS with the SDK",
+    link: "https://www.coursera.org/account/accomplishments/verify/AC5R3PMKCVR4",
   },
   {
     id: 3,
     image: Work3,
-    title: "Introduction to Android Mobile Application Development",
-    link: "https://www.coursera.org/account/accomplishments/verify/SYMAHGJZLP7E?utm_source=ln&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course",
+    title: "Postman API Fundamentals Student Expert",
+    link: "https://badgr.com/public/assertions/eITuYwlOR12xKbFdUPC6jg?identity__email=dasithjaya99@gmail.com",
   },
   {
     id: 4,
     image: Work4,
-    title: "Version Control",
-    link: "https://www.coursera.org/account/accomplishments/verify/KKHXZAVZ7WVU?utm_source=ln&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course",
+    title: "Serverless Firebase Development",
+    link: "https://www.cloudskillsboost.google/public_profiles/586bee6d-8079-42e3-9949-b1adff81b189/badges/4778184?utm_medium=social&utm_source=linkedin&utm_campaign=ql-social-share",
   },
   {
     id: 5,
     image: Work5,
+    title: "Introduction to Android Mobile Application Development",
+    link: "https://www.coursera.org/account/accomplishments/verify/SYMAHGJZLP7E?utm_source=ln&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course",
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "Version Control",
+    link: "https://www.coursera.org/account/accomplishments/verify/KKHXZAVZ7WVU?utm_source=ln&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course",
+  },
+  {
+    id: 7,
+    image: Work7,
     title:
       ".NET Fundamentals: Concepts, APIs, and Libraries in the .NET Framework",
     link: "https://www.linkedin.com/learning/certificates/fc3ec0e3bde7bd776e195de7e3f7957dcd23b2f6af3edbe0384030268eaec7b0",
   },
   {
-    id: 6,
-    image: Work6,
+    id: 8,
+    image: Work8,
     title: "Programming Fundamentals in Kotlin",
     link: "https://www.coursera.org/account/accomplishments/verify/RQBQGLLNG55B?utm_source=ln&utm_medium=certificate&utm_content=cert_image&utm_campaign=pdf_header_button&utm_product=course",
   },
